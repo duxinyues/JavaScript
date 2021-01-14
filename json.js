44db8cc063fa1012b2a9e0c96607ebc5a9aa8454
qingzhuxin <yongyuan253015@gmail.com>
2021-01-14 19:31:34 +0800

    JSON的另一种解法

--- json.js
+++ json.js
@@ -1,9 +1,16 @@
-const  _json = {
-   name:"duxin",
-   age:28
+const _json = {
+   name: "读心悦",
+   lat: "",
+   age: 28,
+   mon: [120, 234, 31, 654, 90],
+   test: {
+      name: "青竹心"
+   },
 }
-// JSON.parse(_json)
- const _json1 =  JSON.stringify(_json,["name"])
-console.log(_json);
-
-console.log(_json1)
+const _json1 = JSON.stringify(_json)
+const _json2 = JSON.parse(_json1, function (key, value) {
+   if(key=="name"){
+      console.log("909090")
+   }
+})
+console.log(_json2); 
