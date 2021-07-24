f55704f88cad92e1adaf304c5a9e302eac218166
duxinyues <yongyuan253015@gmail.com>
2021-07-24 23:12:13 +0800

    Number类型

--- data-type/index.js
+++ data-type/index.js
@@ -1,17 +1,10 @@
 /*
  * @Author: yongyuan at <yongyuan253015@gmail.com>
  * @Date: 2021-07-24 13:48:37
- * @LastEditTime: 2021-07-24 14:22:51
+ * @LastEditTime: 2021-07-24 19:19:17
  * @LastEditors: yongyuan at <yongyuan253015@gmail.com>
  * @Description: 
  * @FilePath: \JavaScript\data-type\index.js
  * 
  */
 function fn() { }
-
-var aa = null;
-
-
-aa == 203
-
-console.log(aa)
