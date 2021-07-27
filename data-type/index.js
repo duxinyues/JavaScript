e4b51c532bcf39dfca2776714f93449ebb122e87
duxinyues <yongyuan253015@gmail.com>
2021-07-27 22:54:36 +0800

    this的使用

--- data-type/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: yongyuan at <yongyuan253015@gmail.com>
- * @Date: 2021-07-25 17:31:12
- * @LastEditTime: 2021-07-25 17:31:54
- * @LastEditors: yongyuan at <yongyuan253015@gmail.com>
- * @Description: 浮点数加减乘除精度处理
- * @FilePath: \JavaScript\data-type\index.js
- * 
- */
---
+++ data-type/string2.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: yongyuan at <yongyuan253015@gmail.com>
+ * @Date: 2021-07-25 23:23:22
+ * @LastEditTime: 2021-07-27 22:49:09
+ * @LastEditors: yongyuan at <yongyuan253015@gmail.com>
+ * @Description: 
+ * @FilePath: \JavaScript\data-type\string2.js
+ * 
+ */
+function Person(name) {
+    this.name = name;
+}
+
+const test = new Person("duxin");
+
+console.log(test.name)
+console.log(window.name)
