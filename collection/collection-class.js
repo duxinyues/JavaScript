cc9ce96bae9a0170dce2c5cd4778a4b15a268db2
duxinyues <yongyuan253015@gmail.com>
2021-07-30 23:53:02 +0800

    集合

---
+++ collection/collection-class.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: yongyuan at <yongyuan253015@gmail.com>
+ * @Date: 2021-07-30 23:21:43
+ * @LastEditTime: 2021-07-30 23:23:19
+ * @LastEditors: yongyuan at <yongyuan253015@gmail.com>
+ * @Description: 集合的类声明
+ * @FilePath: \JavaScript\collection\collection-class.js
+ * 
+ */
+class  Set{
+    constructor(){
+        this.items = {}; // 用对象来表示集合
+    }
+}
--- data-type/string2.js
+++ data-type/string2.js
@@ -1,17 +1,43 @@
 /*
  * @Author: yongyuan at <yongyuan253015@gmail.com>
  * @Date: 2021-07-25 23:23:22
- * @LastEditTime: 2021-07-27 22:49:09
+ * @LastEditTime: 2021-07-27 23:26:29
  * @LastEditors: yongyuan at <yongyuan253015@gmail.com>
  * @Description: 
  * @FilePath: \JavaScript\data-type\string2.js
  * 
  */
-function Person(name) {
-    this.name = name;
+// function Person(name) {
+//     this.name = name;
+// }
+
+// const test = new Person("duxin");
+
+// console.log(test.name)
+// console.log(window.name)
+
+
+// const value = 100;
+// const obj = {
+//     value: 1,
+//     method: function () {
+//         const foo = function () {
+//             console.log(this.value)
+//             console.log(this)
+//         }
+//         foo();
+//         return this.value;
+//     }
+// }
+// obj.method();
+
+
+function add(x, y) {
+    return x + y;
 }
 
-const test = new Person("duxin");
+function test(x, y) {
+    return add.call(this, x, y)
+}
 
-console.log(test.name)
-console.log(window.name)
+console.log(test(10, 20))
