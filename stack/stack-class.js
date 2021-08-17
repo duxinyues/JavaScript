2b72d5a2919456144877b2e9ab3800edb26f8fe5
duxinyues <yongyuan253015@gmail.com>
2021-08-17 22:54:01 +0800

    类声明栈

--- stack/stack-class.js
+++ stack/stack-class.js
@@ -2,12 +2,41 @@
  * @Author: yongyuan253015@gmail.com
  * @Date: 2021-08-17 22:28:23
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-08-17 22:29:24
+ * @LastEditTime: 2021-08-17 22:52:58
  * @Description: 类表示栈
  */
 
-class  Stack {
-    constructor(){
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+    push(element) {
+        this.items.push(element);
+    }
+    pop() {
+        return this.items.pop()
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+    isEmpty() {
+        return this.items.length === 0;
+    }
+    size() {
+        return this.items.length;
+    }
+    clear() {
         this.items = [];
     }
 }
+
+// 验证栈
+
+let stack = new Stack()
+
+console.log(stack.isEmpty())
+stack.push(43);
+stack.push(90);
+
+console.log(stack.size())
