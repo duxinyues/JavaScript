83d4a9d08e8e747a792ce835e4c087a6b0bccd3f
duxinyues <yongyuan253015@gmail.com>
2021-09-01 23:10:59 +0800

    深浅拷贝

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Author: yongyuan253015@gmail.com
  * @Date: 2021-08-23 21:40:21
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-08-30 23:15:14
+ * @LastEditTime: 2021-08-30 23:54:55
  * @Description: 文件描述
  */
 // import LinkedList from "./link/linkedList";
@@ -10,26 +10,17 @@
 // const list = new LinkedList();
 
 // list.push(90);
-
-var number = 10;
-function Person() {
-    number = 20;
-    this.number = 1000;
+function add(a, b) {
+    return a + b;
 }
 
-Person.prototype.get = function () {
-    return this.number + 90;
+function test(x, y) {
+    const aa = add.bind(this, x, y);
+    return aa();
+    // return add.call(this, x, y);
 }
-
-var s = new Person();
-console.log(s.get());
-var age = 1093;
-var obj = {
-    age: 10
+function add1(x, y) {
+    return add.apply(this, [x, y])
 }
-
-const test = function () {
-    console.log(this.age);
-}
-
-test()
+console.log(test(90, 10)); // 100
+console.log(add1(9, 10)); // 100
