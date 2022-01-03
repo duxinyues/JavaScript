afb2c7796816c64d680ef8c2ea45093a4aa5e6a8
读心悦 <dxdexin@outlook.com>
2022-01-03 13:28:31 +0800

    feat：动画的触目位置

--- animation/js/utils.js
+++ animation/js/utils.js
@@ -2,7 +2,7 @@
  * @Author: yongyuan253015@gmail.com
  * @Date: 2022-01-03 12:39:14
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-01-03 13:11:24
+ * @LastEditTime: 2022-01-03 13:26:08
  * @Description: 文件描述
  */
 const utils = {
@@ -10,6 +10,7 @@ const utils = {
     captureMouse: (element) => {
         let mouse = { x: 0, y: 0 };
         element.addEventListener('mousemove', (event) => {
+            console.log("event", event, document)
             let x, y;
             if (event.pageX || event.page.Y) {
                 x = event.pageX;
@@ -29,7 +30,36 @@ const utils = {
     },
 
     // 触摸位置
-    captureTouch: (element) => { 
-        let touch ={x:null,t:null,isPressed:false}
+    captureTouch: (element) => {
+        /**
+         * isPressed 是否按下
+         */
+        let touch = { x: null, y: null, isPressed: false };
+
+        element.addEventListener('touchstart', () => {
+            touch.isPressed = true;
+        }, false);
+
+        element.addEventListener('touchend', () => {
+            touch.isPressed = false;
+            touch.x = null;
+            touch.y = null;
+        }, false);
+
+        element.addEventListener('touchmove', (e) => {
+            let x, y, touch_event = e.touches[0];
+            if (touch_event.pageX || touch_event.pageY) {
+                x = touch_event.pageX;
+                y = touch_event.pageY;
+            } else {
+                x = touch_event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+                y = touch_event.clientY + document.body.scrollTop + document.documentURI.scrollTop;
+            }
+
+            x -= element.offsetLeft;
+            y -= element.offsetTop;
+        }, false);
+
+        return touch
     }
 }
