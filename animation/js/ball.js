c6e98abec0fb04aa08c3fe9ada6c7565e7ce1244
读心悦 <dxdexin@outlook.com>
2022-01-04 20:18:43 +0800

    三角函数动画

---
+++ animation/js/ball.js
@@ -0,0 +1,37 @@
+/*
+ * @Author: yongyuan253015@gmail.com
+ * @Date: 2022-01-03 22:47:13
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-01-03 23:21:48
+ * @Description: 文件描述
+ */
+
+function Ball(radius, color) {
+    this.radius = radius || 150;
+    this.color = color || "red";
+    this.x = 0;
+    this.y = 0;
+    this.rotation = 0;
+    this.scaleX = 1;
+    this.scaleY = 1;
+    this.lineWidth = 3;
+}
+
+
+Ball.prototype.draw = function (context) {
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(this.rotation);
+    context.scale(this.scaleX, this.scaleY);
+    context.lineWidth = this.lineWidth;
+    context.fillStyle = this.color;
+    context.beginPath();
+    context.arc(0, 0, this.radius, 0, Math.PI * 2, true);
+    context.closePath();
+    context.fill();
+
+    if (this.lineWidth > 0) {
+        context.stroke();
+    }
+    context.restore();
+}
