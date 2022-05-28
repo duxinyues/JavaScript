f38b6bf540a005cda240ba4e13573cf57c9faddd
duxinyues <yongyuan253015@gmail.com>
2022-05-28 17:08:50 +0800

    平衡二叉树: AVLTree

--- datastructures-algorithms/tree/index.js
+++ datastructures-algorithms/tree/index.js
@@ -137,25 +137,172 @@ class BinarySearchTree {
                 return node;
             }
 
-            if(node.left==null){
+            if (node.left == null) {
                 node = node.right;
                 return node;
             }
 
-            if(node.right == null){
+            if (node.right == null) {
                 node = node.left;
                 return node
             }
 
             const aux = this.minNode(node.right)
             node.key = aux.key;
-            node.right = this.removeNode(node.right,aux.key);
+            node.right = this.removeNode(node.right, aux.key);
             return node;
         }
     }
 }
 
+/**
+ * 
+ * 树的一条分支有很多层的时候，其他的分支层数很少，
+ * 这样在添加、删除和搜索节点的时候，引起一些性能的问题
+ * 自平衡树：就是任何一个节点的左右两侧子树的高度相差最多为1。
+ */
+class AVLTree extends BinarySearchTree {
+    constructor(compareFn = defaultCompare) {
+        super(compareFn);
+        this.compareFn = compareFn;
+        this.root = null;
+    }
 
+    // 计算节点的高度，返回最高的节点
+    getNodeHeight(node) {
+        if (node == null) {
+            return -1;
+        }
+
+        return Math.max(this.getNodeHeight(node.left), this.getNodeHeight(node.right))
+    }
+
+    // 计算一个平衡因子
+    getBalanceFactor(node) {
+        /**
+         * 平衡操作：
+         * 在添加或者删除节点后，我们需要计算节点的高度并且验证树是否需要进行平衡
+         */
+        const heightDifference = this.getNodeHeight(node.left) - this.getNodeHeight(node.right);
+
+        switch (heightDifference) {
+            case -2:
+                return BalanceFactor.UNBALANCED_RIGHT;
+            case -1:
+                return BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT;
+            case 1:
+                return BalanceFactor.SLIGHTLY_UNBALANCED_LEFT;
+            case 2:
+                return BalanceFactor.UNBALANCED_LEFT;
+            default:
+                return BalanceFactor.BALANCED;
+        }
+    }
+
+    // 左左：向右单向旋转
+    rotationLL(node) {
+        const tmp = node.left;
+        node.left = tmp.right;
+        tmp.right = node;
+        return tmp;
+    }
+
+    // 右右：向左单向旋转
+    rotationRR(node) {
+        const tmp = node.right;
+        node.right = tmp.left;
+        tmp.left = node;
+        return tmp;
+    }
+
+    // 左右：向右双旋转
+    rotationLR(node) {
+        node.left = this.rotationRR(node.left);
+        return this.rotationLL(node);
+    }
+
+    // 右左：向左双旋转
+    rotationRL(node) {
+        node.right = this.rotationLL(node.right);
+        return thid.rotationRR(node)
+    }
+
+    insert(key) {
+        this.root = this.insertNode(this.root, key)
+    }
+    insertNode(node, key) {
+        if (node == null) {
+            return new Node(key)
+        } else if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            node.left = this.insertNode(node.left, key);
+        } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+            node.right = this.insertNode(node.right, key);
+        } else {
+            return node;
+        }
+
+        // 平衡操作
+        const balanceFactor = this.getBalanceFactor(node);
+
+        if (balanceFactor === BalanceFactor.UNBALANCED_LEFT) {
+            if (this.compareFn(key, node.left.key) === Compare.LESS_THAN) {
+                node = this.rotationLL(node);
+            } else {
+                return this.rotationLR(node);
+            }
+        }
+
+        if (balanceFactor === BalanceFactor.UNBALANCED_RIGHT) {
+            if (this.compareFn(key, node.right.key) === Compare.BIGGER_THAN) {
+                node = this.rotationRR(node);
+            } else {
+                return this.rotationRL(node)
+            }
+        }
+
+        return node
+    }
+
+    removeNode(node, key) {
+        node = super.removeNode(node, key);
+
+        if (node === null) {
+            return node;
+        }
+        // 需要平衡
+        const balanceFactor = this.getBalanceFactor(node);
+        if (balanceFactor === BalanceFactor.UNBALANCED_LEFT) {
+            const balanceFactorLeft = this.getBalanceFactor(node.left);
+            if (balanceFactorLeft === BalanceFactor.BALANCED || balanceFactorLeft === BalanceFactor.SLIGHTLY_UNBALANCED_LEFT) {
+                return this.rotationLL(node);
+            }
+
+            if (balanceFactorLeft === BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT) {
+                return this.rotationLR(node.left)
+            }
+        }
+
+        if (balanceFactor === BalanceFactor.UNBALANCED_RIGHT) {
+            const balanceFactorRight = this.getBalanceFactor(node.right);
+            if (balanceFactorRight === BalanceFactor.BALANCED || balanceFactorRight === BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT) {
+                return this.rotationRR(node)
+            }
+
+            if (balanceFactorRight === BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT) {
+                return this.rotationRL(node.right)
+            }
+        }
+        return node
+    }
+}
+
+const BalanceFactor = {
+    UNBALANCED_RIGHT: 1,
+    SLIGHTLY_UNBALANCED_RIGHT: 2,
+    BALANCED: 3,
+    SLIGHTLY_UNBALANCED_LEFT: 4,
+    UNBALANCED_LEFT: 5
+}
 const tree = new BinarySearchTree();
 
 tree.insert(32);
